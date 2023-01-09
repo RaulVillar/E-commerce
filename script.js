d071d4e9e8cc69cf8bed46a8763b91d389e7f9fb
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-01-09 14:25:38 +0100

    render branch

---
+++ script.js
@@ -0,0 +1,10 @@
+function renderProducts() {
+    products.forEach((info) => {
+        const miArticle = document.createElement("div"); //Estructura
+        miArticle.classList.add("card", "col-sm-4");
+
+        const miArticleCardBody = document.createElement ("div")
+
+    });
+}
+       
