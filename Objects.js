d960ad85f37175eced8cef36728ab2c5377e8ca1
Raul Villar <raultheviking@gmail.com>
2023-01-11 11:55:40 +0100

    Working on Local Storage

--- Objects.js
+++ Objects.js
@@ -3,56 +3,37 @@ const products = [
         id: 1,
         name: "Refresco de Sailor Moon",
         price: 1.50,
-        description: "Un producto la mar de refrescante, sabor a pomelo y con la grandiosa Sailor Moon como protagonista",
+        description: "Un producto la mar de refrescante, sabor a pomelo y con la grandiosa Sailor Moon como protagonista.",
         image: 'imagenes/prd-agua-soda-sabor-pomelo-edicion-sailor-moon-japonshop_2.webp',
-        category: [
-            "Products",
-            "New"
-        ]
+        category: "New",
+        quantity: 0
     },
     {
         id: 2,
-        name: "Bebida de café Asahi Wonda Premium",
+        name: "Bebida de cafe Asahi Wonda Premium",
         price: 1.50,
-        description: "El café de la marca Asahi, fabricado por japoneses y para japoneses, es un producto exclusivo que acompaña a estudiantes y oficinistas en sus largas jornadas de exhaustivo trabajo.",
+        description: "El cafe de la marca Asahi, fabricado por japoneses y para japoneses, es un producto exclusivo.",
         image: 'imagenes/prd-bebida-cafe-asahi-wonda-gold-premium-1-japonshop_2.webp',
-        category: [
-            "Products",
-            "New"
-        ]
+        category: "New",
+        quantity: 0
     },
     {
         id: 3,
-        name: "Bombones de melocotón, frambuesa y chocolate",
+        name: "Bombones de chocolate",
         price: 3,
-        description: "Una explosión de sabor inigualable, un producto exquisito fabricado por los más expertos chocolateros nipones. Pruébalo",
+        description: "Una explosion de sabor inigualable, un producto exquisito fabricado por los más expertos chocolateros nipones.",
         image: 'imagenes/prd-bombones-petit-melo-chocolate-frambuesa-japonshop.webp',
-        category: [
-            "Products",
-            "New"
-        ]
+        category: "New",
+        quantity: 0
     },
     {
         id: 4,
-        name: "Chicles de One Piece",
+        name: "Chicles de melocoton One Piece",
         price: 2.50,
-        description: "Chicles sabor melocotón, no te concederán poderes ni te servirán para encontrar el One Piece, pero están riquísimos.",
+        description: "Chicles sabor melocoton, no te concederan poderes, pero estan riquisimos.",
         image: 'imagenes/prd-chicles-one-piece-japon-japonshop.webp',
-        category: [
-            "Products",
-            "New"
-        ]
-    },
-    {
-        id: 5,
-        name: "Crema de caramelo japonesa",
-        price: 5,
-        description: "Una crema especialmente cremosa, con un sabor no excesivamente dulce. Es adictiva",
-        image: 'imagenes/prd-crema-coco-caramelo-japonshop.webp',
-        category: [
-            "Products",
-            "New"
-        ]
+        category: "New",
+        quantity: 0
     },
     {
         id: 6,
@@ -60,36 +41,35 @@ const products = [
         price: 2,
         description: "Una bolsita de fideos, sabor a pollo, con los ingredientes preferidos de nuestro amado Naruto.",
         image: 'imagenes/prd-fideos-ramen-naruto-yile-setas-japon-japonshop_1.webp',
-        category: [
-            "Products",
-            "New"
-        ]
+        category: "Products",
+        quantity: 0
     },
     {
         id: 7,
         name: "Puzle de Sanrio",
         price: 6,
-        description: "El puzle de Sanrio no es comestible, pero es adorable y muy entretenido de montar *No ingerir",
+        description: "El puzle de Sanrio no es comestible, pero es adorable y muy entretenido de montar.",
         image: 'imagenes/prd-diy-puzle-sanrio-friends-japonshop.webp',
-        category: "Products"
-    
+        category: "Products",
+        quantity: 0 
     },
     {
         id: 8,
-        name: "Galleta Pokemon Fresa",
+        name: "Galleta Pokemon",
         price: 3,
         description: "Una adorable galletita con un pokemon sorpresa. Hay mas de 20 misteriosos pokemons por comer!",
         image: 'imagenes/prd-galleta-pokemon-fresa-japonshop.webp',
-        category: "Products"
-         
+        category: "Products",
+        quantity: 0
     },
     {
         id: 9,
-        name: "Ramen de pollo Chikin sabor pizza",
+        name: "Ramen Chikin",
         price: 3,
         description: "Como lo escuchas, un ramen sabor a pizza cuatro quesos procedente de la famosa marca Chikin.",
         image: 'imagenes/prd-ramen-pollo-chikin-pizza-japonshop_1.webp',
-        category: "Products"
+        category: "Products",
+        quantity: 0
     },
     {
         id: 10,
@@ -97,23 +77,26 @@ const products = [
         price: 3.20,
         description: "Esta salsa de curry, con cerdo y maiz, que por suerte no ha cocinado Sinchan, es suave al paladar y muy nutritiva.",
         image: 'imagenes/prd-salsa-curry-japonesa-cerdo-maiz-shin-chan-japonshop_1.webp',
-        category: "Exclusive"
+        category: "Exclusive",
+        quantity: 0
     },
     {
         id: 11,
         name: "Salsa de curry de Doraemon",
         price: 3.20,
-        description: "Una salsa de curry, con cerdo, verduras y queso, que se ha sacado Doraemon del bolsillo mágico.",
+        description: "Una salsa de curry, con cerdo, verduras y queso, que se ha sacado Doraemon del bolsillo magico.",
         image: 'imagenes/prd-salsa-curry-japonesa-cerdo-verduras-queso-doraemon-japonshop_1.webp',
-        category: "Exclusive"
+        category: "Exclusive",
+        quantity: 0
     },
     {
         id: 12,
         name: "Sopa de maiz de Pokemon",
         price: 1.75,
-        description: "Nutritiva sopita de maiz con temática de Pokemon. *No incluye trazas de pokemons",
+        description: "Nutritiva sopita de maiz con tematica de Pokemon. *No incluye trazas de pokemons",
         image: 'imagenes/prd-sopa-maiz-pokemon-japonesa-japonshop.webp',
-        category: "Exclusive"
+        category: "Exclusive",
+        quantity: 0
     },
     {
         id: 13,
@@ -121,14 +104,8 @@ const products = [
         price: 5,
         description: "Una imperial cebada nipona, limitada y exclusiva, del osito Rilakkum",
         image: 'imagenes/prd-te-cebada-japones-edicion-limitada-rilakkuma-japonshop.webp',
-        category: "Exclusive"
+        category: "Exclusive",
+        quantity: 0
     },
-    {
-        id: 14,
-        name: "Ramen de Udon coreano",
-        price: 4.20,
-        description: "¿Por qué hay ramen de Udon coreano en una página de productos japoneses? Yo que sé.",
-        image: 'imagenes/prd-udon-coreano-big-nongshim-big-japonshop.webp',
-        category: "Exclusive"
-    },
+   
 ]
--- script.js
+++ script.js
@@ -1,110 +1,4 @@
-const products = [
-    {
-        id: 1,
-        name: "Refresco de Sailor Moon",
-        price: 1.50,
-        description: "Un producto la mar de refrescante, sabor a pomelo y con la grandiosa Sailor Moon como protagonista.",
-        image: 'imagenes/prd-agua-soda-sabor-pomelo-edicion-sailor-moon-japonshop_2.webp',
-        category: "New"
-    },
-    {
-        id: 2,
-        name: "Bebida de cafe Asahi Wonda Premium",
-        price: 1.50,
-        description: "El cafe de la marca Asahi, fabricado por japoneses y para japoneses, es un producto exclusivo.",
-        image: 'imagenes/prd-bebida-cafe-asahi-wonda-gold-premium-1-japonshop_2.webp',
-        category: "New"
-
-    },
-    {
-        id: 3,
-        name: "Bombones de chocolate",
-        price: 3,
-        description: "Una explosion de sabor inigualable, un producto exquisito fabricado por los más expertos chocolateros nipones.",
-        image: 'imagenes/prd-bombones-petit-melo-chocolate-frambuesa-japonshop.webp',
-        category: "New"
-
-    },
-    {
-        id: 4,
-        name: "Chicles de melocoton One Piece",
-        price: 2.50,
-        description: "Chicles sabor melocoton, no te concederan poderes, pero estan riquisimos.",
-        image: 'imagenes/prd-chicles-one-piece-japon-japonshop.webp',
-        category: "New"
-
-    },
-    {
-        id: 6,
-        name: "Fideos Ramen Naruto",
-        price: 2,
-        description: "Una bolsita de fideos, sabor a pollo, con los ingredientes preferidos de nuestro amado Naruto.",
-        image: 'imagenes/prd-fideos-ramen-naruto-yile-setas-japon-japonshop_1.webp',
-        category: "Products"
-
-    },
-    {
-        id: 7,
-        name: "Puzle de Sanrio",
-        price: 6,
-        description: "El puzle de Sanrio no es comestible, pero es adorable y muy entretenido de montar.",
-        image: 'imagenes/prd-diy-puzle-sanrio-friends-japonshop.webp',
-        category: "Products"
-
-    },
-    {
-        id: 8,
-        name: "Galleta Pokemon",
-        price: 3,
-        description: "Una adorable galletita con un pokemon sorpresa. Hay mas de 20 misteriosos pokemons por comer!",
-        image: 'imagenes/prd-galleta-pokemon-fresa-japonshop.webp',
-        category: "Products"
-
-    },
-    {
-        id: 9,
-        name: "Ramen Chikin",
-        price: 3,
-        description: "Como lo escuchas, un ramen sabor a pizza cuatro quesos procedente de la famosa marca Chikin.",
-        image: 'imagenes/prd-ramen-pollo-chikin-pizza-japonshop_1.webp',
-        category: "Products"
-    },
-    {
-        id: 10,
-        name: "Salsa de curry de Sinchan",
-        price: 3.20,
-        description: "Esta salsa de curry, con cerdo y maiz, que por suerte no ha cocinado Sinchan, es suave al paladar y muy nutritiva.",
-        image: 'imagenes/prd-salsa-curry-japonesa-cerdo-maiz-shin-chan-japonshop_1.webp',
-        category: "Exclusive"
-    },
-    {
-        id: 11,
-        name: "Salsa de curry de Doraemon",
-        price: 3.20,
-        description: "Una salsa de curry, con cerdo, verduras y queso, que se ha sacado Doraemon del bolsillo magico.",
-        image: 'imagenes/prd-salsa-curry-japonesa-cerdo-verduras-queso-doraemon-japonshop_1.webp',
-        category: "Exclusive"
-    },
-    {
-        id: 12,
-        name: "Sopa de maiz de Pokemon",
-        price: 1.75,
-        description: "Nutritiva sopita de maiz con tematica de Pokemon. *No incluye trazas de pokemons",
-        image: 'imagenes/prd-sopa-maiz-pokemon-japonesa-japonshop.webp',
-        category: "Exclusive"
-    },
-    {
-        id: 13,
-        name: "Cebada japonesa limitada Rilakkum",
-        price: 5,
-        description: "Una imperial cebada nipona, limitada y exclusiva, del osito Rilakkum",
-        image: 'imagenes/prd-te-cebada-japones-edicion-limitada-rilakkuma-japonshop.webp',
-        category: "Exclusive"
-    },
-   
-]
-
-
+let carrito = [];
 const divisa = '€';
 const DOMnew = document.querySelector('#new');
 const DOMproducts = document.querySelector('#products');
@@ -113,27 +7,27 @@ const DOMexclusive = document.querySelector('#exclusive');
 
 function renderNew() {
 
-    var news = products.filter(item => item.category == "New")
-    news.forEach((item) => { printNew(item) })
+    var news = products.filter(item => item.category == "New");
+    news.forEach((item) => { printNew(item) });
 
 }
 
 function renderProducts() {
 
-    var product = products.filter(item => item.category == "Products")
-    product.forEach((item) => { printProduct(item) })
+    var product = products.filter(item => item.category == "Products");
+    product.forEach((item) => { printProduct(item) });
 
 }
 
 function renderExclusive() {
 
-    var exclusive = products.filter(item => item.category == "Exclusive")
-    exclusive.forEach((item) => { printExclusive(item) })
+    var exclusive = products.filter(item => item.category == "Exclusive");
+    exclusive.forEach((item) => { printExclusive(item) });
 
 }
 
-renderProducts()
 renderNew()
+renderProducts()
 renderExclusive()
 
 function printNew(item) {
@@ -157,20 +51,20 @@ function printNew(item) {
     const myProductPrice = document.createElement("p");
     myProductPrice.innerText = item.price + divisa;
     myProductPrice.id = 'product-price';
-   
-   const myProductButton = document.createElement("button");
-   myProductButton.innerHTML = "Shop"
-   myProductButton.id='product-button';
-  
-  
+
+    const myProductButton = document.createElement("button");
+    myProductButton.innerHTML = "Shop"
+    myProductButton.id = 'product-button';
+    myProductButton.setAttribute('marker', item);
+    myProductButton.addEventListener('click', shoppingButton);
 
     myProduct.appendChild(myProductTitle);
     myProduct.appendChild(myProductImage);
     myProduct.appendChild(myProductDescription);
     myProduct.appendChild(myProductPrice);
     myProduct.appendChild(myProductButton);
-    DOMnew.appendChild(myProduct)
-    
+    DOMnew.appendChild(myProduct);
+
 }
 
 function printProduct(item) {
@@ -197,14 +91,16 @@ function printProduct(item) {
 
     const myProductButton = document.createElement("button");
     myProductButton.innerHTML = "Shop"
-    myProductButton.id='product-button';
+    myProductButton.id = 'product-button';
+    myProductButton.setAttribute('marker', item);
+    myProductButton.addEventListener('click', shoppingButton);
 
     myProduct.appendChild(myProductTitle);
     myProduct.appendChild(myProductImage);
     myProduct.appendChild(myProductDescription);
     myProduct.appendChild(myProductPrice);
     myProduct.appendChild(myProductButton);
-    DOMproducts.appendChild(myProduct)
+    DOMproducts.appendChild(myProduct);
 }
 
 
@@ -232,17 +128,36 @@ function printExclusive(item) {
 
     const myProductButton = document.createElement("button");
     myProductButton.innerHTML = "Shop"
-    myProductButton.id='product-button';
+    myProductButton.id = 'product-button';
+    myProductButton.setAttribute('marker', item);
+    myProductButton.addEventListener('click', shoppingButton);
 
     myProduct.appendChild(myProductTitle);
     myProduct.appendChild(myProductImage);
     myProduct.appendChild(myProductDescription);
     myProduct.appendChild(myProductPrice);
     myProduct.appendChild(myProductButton);
-    DOMexclusive.appendChild(myProduct)
+    DOMexclusive.appendChild(myProduct);
 }
 
-let carrito
 
-addEventListener(click)
-carrito.push
+
+
+
+
+
+
+function shoppingButton(event) {
+
+var shoppingProduct = {}
+
+shoppingProduct.id = `${event.id}`
+shoppingProduct.image = `${event.image}`
+shoppingProduct.name = `${event.name}`
+shoppingProduct.price = `${event.price}`
+
+console.info (shoppingProduct)
+    carrito.push (shoppingProduct)
+    // carrito.push(event.target.getAttribute('marker'));
+    console.info (carrito);
+}
