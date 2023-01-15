41e1dafb8dd85f297dffb203d98aa98b54688ca4
Carlos Sánchez <holapueblodev@gmail.com>
2023-01-15 16:47:43 +0100

    delete token

--- js/API.respond.js
+++ js/API.respond.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 4000
 
 app.get('/', (request, response) => {
-  response.send('EAAGEnv1Ou5kBACwk2IPH2bKUrFrM7AgpvQI8NZC5UPkWRZC77rMI10CSRpqInwFQFEYXVCOIZA5Bg64bZCS0TmkVpS2ZCO6dAFktRK61rqstkFPaJZBfGKiAtWZCWiZBuZB4Qx1UJpJlI7FOsFXUHEwbSd31JcsqLM2ZAgngHB5mQXjzjQ3rFaSNrWcSpywGBszkzViOT66YJ0xwZDZD')
+  response.send('')
 })
